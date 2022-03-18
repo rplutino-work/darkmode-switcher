173588888e79c2d2ba521a8e44e1cd900fd9ef69
Rodrigo <rodrigo@pow.la>
2022-03-18 15:07:20 -0300

    first commit

---
+++ react/darkmodeSwitcher.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {useState} from 'react'
+
+function darkmodeSwitcher({ name }: Props) {
+
+  const [classDark, setClassDark] = useState("vtex-flex-layout-0-x-flexCol--darkMode-Col-1")
+
+  handleDark = () =>{
+
+  setClassDark("vtex-flex-layout-0-x-flexCol--darkMode-Col-dark-1")
+
+  }
+  return (
+    <div>
+      <div className={classDark} onClick={handleDark}>
+        <div className="vtex-flex-layout-0-x-flexColChild--darkMode-Col-1">icon 1 </div>
+        <div className="vtex-flex-layout-0-x-flexColChild--darkMode-Col-1">icon 2</div>
+      </div>
+    </div>
+  )
+}
+
+export default darkmodeSwitcher
